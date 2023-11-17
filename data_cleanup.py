f196ad6d2b69e286ddb87a623a2f37e421c5346b
HLIN9 <han.lin1@ucalgary.ca>
2023-11-17 16:56:36 -0700

    All data cleaned

--- data_cleanup.py
+++ data_cleanup.py
@@ -36,7 +36,42 @@ try:
     df = pd.read_excel(file_path, sheet_name=sheet_name, usecols=columns_of_interest)
 
     # Display the first few rows 
-    print(df.head())
+    # print(df.head())
+
+    #Change to platform of analysis
+    platform = 'TikTok'
+
+    # Get columns containing 'Twitter' in their names
+    platform_columns = [col for col in df.columns if platform in col]
+
+    # Keep the columns related to Twitter along with specified columns
+    columns_to_keep = [
+        'Name (English)',
+        'Region of Focus',
+        'Language',
+        'Entity owner (English)',
+        'Parent entity (English)'
+    ] + platform_columns
+
+    # Filter the DataFrame to keep only specified columns
+    df_platform = df[columns_to_keep]
+
+    # Remove rows with any empty entries
+    df_platform = df_platform.dropna()
+
+    # Double check if desired items are contained
+    print(df_platform.columns.to_list)
+    print(df_platform.shape)
+
+    # Saves the cleaned-files
+    output_file_path = f'cleaned_data_{platform}.csv'
+    df_platform.to_csv(output_file_path, index=False)
+    print(f"Cleaned data saved to '{output_file_path}'")
+
+    # Double check:
+    # tester = pd.read_csv(output_file_path)
+    # print(tester.shape)
+    
 
 
 except FileNotFoundError:
